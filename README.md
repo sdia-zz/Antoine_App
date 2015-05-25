05f4159f82a7b298ddaa1d248183144048b24855
Seydou Dia <seydou.dia@lamudi.com>
2015-05-25 02:21:44 +0200

    formatting

--- README.md
+++ README.md
@@ -10,16 +10,14 @@ Note how Flask handles text encoding :)
 ## Usage
 
 ``` bash
-   virtualenv env
-   . env/bin/activate
-   (env)$ pip install -r requirements.txt
-   (env)$ python app.py
+$ virtualenv env
+$ . env/bin/activate
+(env)$ pip install -r requirements.txt
+(env)$ python app.py
 
-   ... Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
-   ... Restarting with stat
+... Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+... Restarting with stat
 
 ```
 
-
 Point your browser to `` http://127.0.0.1:5000/`` and that's it !
-
--- app.py
+++ app.py
@@ -2,10 +2,27 @@
 #-*- coding:utf-8 -*-
 
 
+# How to merge file on server side using Python's Flask
+# A demo for Antoine
+
+# Copyright (C) 2015 Seydou Dia
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
 from flask import Flask, request, session, render_template
-
-
-DEBUG = True
 
 
 app = Flask(__name__)
@@ -21,7 +38,6 @@ def merge(s, t):
 
 
 def is_valid(s, t):
-
     # validation
     black_list = None, ''
     if s in black_list and t in black_list:
@@ -29,19 +45,16 @@ def is_valid(s, t):
 
     return True
     
-    
 
 # home page
 @app.route('/', methods=['GET', 'POST'])
 def home():
-
     result = dict(
         text1 = '',
         text2 = '',
         text3 = '')
 
     if request.method == 'POST':
-
         # get data here
         text1 = request.form['text1']
         text2 = request.form['text2']
@@ -59,7 +72,7 @@ def home():
 
     return render_template('home.html', result=result)
 
-if __name__ == '__main__':
 
-    app.debug = True # remove in prod
+if __name__ == '__main__':
+    app.debug = True # remove in prod.
     app.run()
